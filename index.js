6e95ff9d3f74aef0d07a97d5f759dcf85e04b586
Sophie Muchiri <muthonisophie12@gmail.com>
2024-07-23 16:53:08 +0300

    scoping

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+var bestCustomer
+
+const LeastFavoriteCustomer = 'steve'
+
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+ }
+ 
+ function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer = 'bonnie'
+ }
+ 
